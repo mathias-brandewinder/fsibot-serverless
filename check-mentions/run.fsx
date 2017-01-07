2d5c0965d8472d3d3d11efa2ff00705d1efecf2a
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2017-01-07 16:35:55 +0100

    Added function to check Twitter mentions

---
+++ check-mentions/run.fsx
@@ -0,0 +1,101 @@
+(*
+Every 2 minutes, check Twitter for potential new mentions.
+Enqueue every mention found, and update the last Tweet ID.
+*)
+
+#r @"System.Linq.Expressions"
+open System
+open System.Configuration
+open LinqToTwitter
+open Newtonsoft.Json
+
+type Mention = {
+    StatusID:uint64
+    Text:string
+    Author:string
+    }
+
+let createContext () =
+
+    let appSettings = ConfigurationManager.AppSettings
+    
+    let apiKey = appSettings.["TwitterApiKey"]
+    let apiSecret = appSettings.["TwitterApiSecret"]
+    let accessToken = appSettings.["TwitterAccessToken"]
+    let accessTokenSecret = appSettings.["TwitterAccessTokenSecret"]
+
+    let credentials = SingleUserInMemoryCredentialStore()
+    credentials.ConsumerKey <- apiKey
+    credentials.ConsumerSecret <- apiSecret
+    credentials.AccessToken <- accessToken
+    credentials.AccessTokenSecret <- accessTokenSecret
+    let authorizer = SingleUserAuthorizer()
+    authorizer.CredentialStore <- credentials
+    new TwitterContext(authorizer)
+
+let run (
+    timer:TimerInfo, 
+    mentionsQueue:ICollector<string>, 
+    previousID:string, 
+    updatedID: string byref, 
+    log:TraceWriter) =
+
+    log.Info("Starting to check for new mentions")
+    
+    let context = createContext ()
+   
+    let lastID =
+        if String.IsNullOrEmpty previousID
+        then None
+        else Some(uint64 previousID)
+        
+    log.Info(sprintf "Last ID: %A" lastID)
+
+    let mentions = 
+        match lastID with
+        | None ->
+            query { 
+                for tweet in context.Status do 
+                where (tweet.Type = StatusType.Mentions)
+                select tweet 
+                }
+        | Some(id) ->
+            query { 
+                for tweet in context.Status do 
+                where (tweet.Type = StatusType.Mentions && tweet.SinceID = id)
+                where (tweet.StatusID <> id)
+                select tweet 
+                }
+        |> Seq.toList
+    
+    mentions
+    |> Seq.iter (fun status ->
+    
+        let mention = {
+            StatusID = status.StatusID
+            Text = status.Text
+            Author = status.User.ScreenNameResponse
+            }
+            
+        let msg =    
+            mention
+            |> JsonConvert.SerializeObject
+        
+        mentionsQueue.Add(msg)
+            
+        log.Info(msg))
+
+    // update the last ID processed
+    let updatedLastID =
+        match mentions with
+        | [] -> lastID
+        | hd::_ -> hd.StatusID |> Some
+        
+    if (updatedLastID <> lastID) 
+    then 
+        match updatedLastID with
+        | None -> ignore ()
+        | Some id -> 
+            updatedID <- string id
+
+    log.Info("Completed checking for new mentions")
